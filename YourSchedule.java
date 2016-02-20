3b7703fe16468aa74a8ec9a664bb3e175e549f76
Fred Luartes <fjluartes@gmail.com>
2016-02-20 15:01:50 +0800

    uploaded java file/s
    uploaded YourSchedule.java

---
+++ YourSchedule.java
@@ -0,0 +1,36 @@
+// Fred Luartes, 2/20/2016
+// Your schedule 
+
+public class YourSchedule 
+{
+	public static void main(String [] args)
+	{
+		String course1, course2, course3, course4, course5, course6, course7, teacher1, teacher2, teacher3, teacher4, teacher5, teacher6, teacher7;
+		
+		course1 = "Calculus";
+		course2 = "English";
+		course3 = "Physics";
+		course4 = "Filipino";
+		course5 = "World History";
+		course6 = "Health";
+		course7 = "Computer Science";
+		
+		teacher1 = "Gauss";
+		teacher2 = "Chomsky";
+		teacher3 = "Newton";
+		teacher4 = "Balagtas";
+		teacher5 = "Friedman";
+		teacher6 = "Rippetoe";
+		teacher7 = "Turing";
+		
+		System.out.println("+-------------------------------------------+"); 
+		System.out.println("|1|   " + course1 + "              |    " + teacher1 + "      |");
+		System.out.println("|2|   " + course2 + "               |    " + teacher2 + "    |");
+		System.out.println("|3|   " + course3 + "               |    " + teacher3 + "     |");
+		System.out.println("|4|   " + course4 + "              |    " + teacher4 + "   |");
+		System.out.println("|5|   " + course5 + "         |    " + teacher5 + "   |");
+		System.out.println("|6|   " + course6 + "                |    " + teacher6 + "   |");
+		System.out.println("|7|   " + course7 + "      |    " + teacher7 + "     |");
+		System.out.println("+-------------------------------------------+"); 	
+	}
+}
