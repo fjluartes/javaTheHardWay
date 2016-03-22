34b2e77f333eb18ce9b301928a760bbeb7d6a2ca
Fred Luartes <fjluartes@gmail.com>
2016-03-22 14:44:43 +0800

    Added files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,28 @@
+// Fred Luartes, 3/22/2016
+// FizzBuzz: This program prints numbers from 1-100. For multiples of 3, print Fizz. For multiples of 5, print Buzz. For multiples of both 3 and 5, print FizzBuzz. 
+
+public class FizzBuzz
+{
+	public static void main(String[] args)
+	{
+		for (int num = 1; num <= 100; num++)
+		{
+			if ((num % 3) == 0 && (num % 5) == 0)
+			{
+				System.out.println("FizzBuzz");
+			}
+			else if ((num % 3) == 0)
+			{
+				System.out.println("Fizz");
+			}
+			else if ((num % 5) == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
+			{
+				System.out.println(num);
+			}
+		}
+	}
+}
