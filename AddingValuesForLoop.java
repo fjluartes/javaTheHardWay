935364390f0dff8d50bbd2cdb3ac88c52416facb
Fred Luartes <fjluartes@gmail.com>
2016-03-23 11:02:43 +0800

    Added files via upload

---
+++ AddingValuesForLoop.java
@@ -0,0 +1,24 @@
+// Fred Luartes, 3/23/2016
+// Adding Values in a For Loop: This program asks the user for an integer, add up all the numbers from 1 to the integer, then displays the total.
+
+import java.util.Scanner;
+
+public class AddingValuesForLoop
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int number, sum;
+		
+		System.out.println("Number: ");
+		number = keyboard.nextInt();
+		sum = 0;
+		
+		for (int i = 1; i <= number; i++)
+		{
+			System.out.println(i);
+			sum = sum + i;
+		}
+		System.out.println("The sum is " + sum);
+	}
+}
