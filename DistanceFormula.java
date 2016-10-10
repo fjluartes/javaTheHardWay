f1ad97a38cae00b58a0ba99ff0ca939c91e2a604
Fred Luartes <fjluartes@gmail.com>
2016-10-10 09:59:59 +0800

    Update DistanceFormula.java

--- DistanceFormula.java
+++ DistanceFormula.java
@@ -15,7 +15,6 @@ public class DistanceFormula {
 	}
  
 	public static double distance( int x1, int y1, int x2, int y2 ) {
-		double d = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2) );
-		return d;
+		return Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2) );
 	}
 }
